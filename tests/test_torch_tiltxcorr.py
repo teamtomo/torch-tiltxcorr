88cb3aaf0abfbc38b89ee9de50a7a2b125c99413
McHaillet <martenchaillet@gmail.com>
2025-12-15 11:47:58 +0100

    update test to allow interpolation of the patch

--- tests/test_torch_tiltxcorr.py
+++ tests/test_torch_tiltxcorr.py
@@ -6,10 +6,10 @@ from torch_tiltxcorr.utils import (
 
 
 def test_shift_detection():
-    a = torch.zeros((10, 10))
-    a[6, 6] = 1
-    b = torch.zeros((10, 10))
-    b[5, 5] = 1
+    a = torch.zeros((24, 24))
+    a[13, 13] = 1
+    b = torch.zeros((24, 24))
+    b[12, 12] = 1
 
     ccc = calculate_cross_correlation(a, a)
     shift = get_shift_from_correlation_image(ccc)
@@ -20,9 +20,9 @@ def test_shift_detection():
     assert dy == pytest.approx(1., 1e-3)
     assert dx == pytest.approx(1., 1e-3)
 
-    b = torch.zeros((10, 10))
-    b[5, 5] = 2.5
-    b[4, 5] = 2.5
+    b = torch.zeros((24, 24))
+    b[12, 12] = 2.5
+    b[11, 12] = 2.5
 
     ccc = calculate_cross_correlation(a, b)
     dy, dx = get_shift_from_correlation_image(ccc)
