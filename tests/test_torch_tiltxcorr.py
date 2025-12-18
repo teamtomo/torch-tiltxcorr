52e430307444f2e7b467a193d9fd26953e4b8758
Marten Chaillet <58044494+McHaillet@users.noreply.github.com>
2025-12-18 17:21:26 +0100

    Refactor get_shift_from_correlation_image function (#32)
    * Refactor get_shift_from_correlation_image function
    
    Refactor get_shift_from_correlation_image function to improve documentation and structure. Added Fourier upsampling for sub-voxel accuracy and adjusted variable names for clarity.
    
    * Implement shift detection tests
    
    Add tests for shift detection using cross-correlation.
    
    * Update utils.py
    
    * Add torch-fourier-rescale to dependencies
    
    * explicit type?
    
    * Remove duplicate test for shift detection in tilt series

--- tests/test_torch_tiltxcorr.py
+++ tests/test_torch_tiltxcorr.py
@@ -15,31 +15,6 @@ from torch_tiltxcorr.utils import (
 )
 
 
-def test_shift_detection():
-    a = torch.zeros((10, 10))
-    a[6, 6] = 1
-    b = torch.zeros((10, 10))
-    b[5, 5] = 1
-
-    ccc = calculate_cross_correlation(a, a)
-    shift = get_shift_from_correlation_image(ccc)
-    assert shift.tolist() == [0.0, 0.0]
-
-    ccc = calculate_cross_correlation(a, b)
-    dy, dx = get_shift_from_correlation_image(ccc)
-    assert dy == pytest.approx(1., 1e-3)
-    assert dx == pytest.approx(1., 1e-3)
-
-    b = torch.zeros((10, 10))
-    b[5, 5] = 2.5
-    b[4, 5] = 2.5
-
-    ccc = calculate_cross_correlation(a, b)
-    dy, dx = get_shift_from_correlation_image(ccc)
-    assert dy == pytest.approx(1.5, 1e-2)
-    assert dx == pytest.approx(1.0, 1e-2)
-
-
 def _generate_shifted_tilt_series(
     shift_magnitude: float = 5.0,
     d: int = 128,
